38846b5d92140294ec5031fa39e372596bb6ef57
iugurchiev <isa.ugurchiev@gmail.com>
2022-11-29 15:09:19 +0100

    Fix init styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonComponent } from './button/button/button.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'button', component: ButtonComponent },
+  { path: 'directive-composition', component: ButtonComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
